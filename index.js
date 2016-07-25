994248034bd8605b015151b2e5e6fed8cd01bdbc
laushinka <laurie.malau@gmail.com>
2016-07-25 17:45:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  var counter = 0;
+  function addBump(){
+    return counter += 1
+  }
+  function getBumps(){
+    return counter
+  }
+  return {
+    addBump,
+    getBumps
+  };
+}
+
+function createAnimal(animalType) {
+  return function(dev) {
+    return {animalType: animalType, deadlyDevice: dev}
+  }
+  // return fn;
+}
+
+sharkCreator = createAnimal("Shark");
+sharkWithFrickinLaserbeam = sharkCreator("Laserbeam")
+sharkWithFrickinCannon = sharkCreator("Cannon")
